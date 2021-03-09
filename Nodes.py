47738d869506006a475b54cceefe6a59b7fca1af
1180300118-luyabin <61092856+1180300118-luyabin@users.noreply.github.com>
2021-03-10 00:57:19 +0800

    对表达式解析加入了更多的算子类，支持了数组切片，注释有改动

--- Nodes.py
+++ Nodes.py
@@ -1,6 +1,3 @@
-from Executor import *
-
-
 class Node:
     # 计算图中节点类的父类
     def __init__(self, type_id, physic_algorithm='relational', **kwargs):
@@ -8,8 +5,6 @@ class Node:
         self.id = kwargs['id']
         self.type_id = type_id
         self.with_grad = kwargs['with_grad']
-        self.out_edges = []
-        self.in_edges = []
 
     def GetId(self):
         return self.id
@@ -17,37 +12,24 @@ class Node:
     def GetType(self):
         return self.type_id
 
-    def __eq__(self, other):
-        if isinstance(other, Node):
-            return (self.id == other.id) and (self.type_id == other.type_id)
-        else:
-            return False
-
-    def __hash__(self):
-        return hash(self.id + self.type_id)
-
-    def __call__(self, executor: Executor):
-        pass
-
 
 # 通过继承实现的其它节点类
 class Root(Node):
     def __init__(self, **kwargs):
         super().__init__(0, **kwargs)
 
-    def __call__(self, executor: Executor):
-        return [edge.end for edge in self.out_edges]
-
 
 # 创建张量所用节点
 class CreateTensor(Node):
     def __init__(self, data_shape, **kwargs):
         super().__init__(1, **kwargs)
-        self.data_shape = eval(data_shape)
-
-    def __call__(self, executor: Executor):
-        executor.graph.output_of_nodes[self] = Tensor(shape=self.data_shape)
-        return [edge.end for edge in self.out_edges]
+        data_shape = data_shape.replace('(', ',').replace(')', ',')
+        data = data_shape.split(',')
+        data_list = []
+        for d in data:
+            if len(d) != 0:
+                data_list.append(int(d))
+        self.data_shape = tuple(data_list)
 
 
 class Val(Node):
@@ -58,133 +40,238 @@ class Val(Node):
     def set_val(self, value):
         self.value = value
 
-    def __call__(self, executor: Executor):
-        tensor = Tensor(shape=(1,))
-        executor.graph.output_of_nodes[self] = tensor.handle = self.value
-        return [edge.end for edge in self.out_edges]
-
 
 class Sql(Node):
-    def __init__(self, t_info, **kwargs):
+    def __init__(self, t_name, **kwargs):
         super().__init__(3, **kwargs)
-        self.t_info = t_info
-
-    def __call__(self, executor: Executor):
-        # TODO:和高斯数据的API
-        pass
+        self.t_name = t_name
 
 
 class Random(Node):
-    def __init__(self, boundary, data_shape, type, **kwargs):
-        super().__init__(4,**kwargs)
-        self.boundary = boundary
-        self.data_shape = eval(data_shape)
-        self.type = type
-
-    def __call__(self, executor: Executor):
-        # TODO
-        # tensor = Tensor(shape=(1,))
-        # graph.output_of_nodes[self] = tensor.handle = self.value
-        pass
-
-
-# 算术表达式所用节点
-class Symbol(Node):
-    def __init__(self, value, **kwargs):
-        super().__init__(5, **kwargs)
-        self.value = value
+    def __init__(self, uLimit, lLimit, **kwargs):
+        super().__init__(4, **kwargs)
+        self.uLimit = uLimit
+        self.lLimit = lLimit
 
 
 # 逻辑控制所用节点
 class Loop(Node):
     def __init__(self, times, loop_id, **kwargs):
-        super().__init__(6, **kwargs)
+        super().__init__(5, **kwargs)
         self.times = times
         self.loop_id = loop_id
-        self.finished_times = 0
-
-    def __call__(self, executor: Executor):
-        end_nodes = [edge.end for edge in self.out_edges]
-        last_nodes = [edge.start for edge in self.in_edges]
-        loop_end_node = None
-        for node in last_nodes:
-            if isinstance(node, LoopEnd) and node.loop_id == self.loop_id:
-                loop_end_node = node
-                break
-        assert loop_end_node is not None, f'Did not find corresponding loop end node for loop node{self.loop_id}'
-        if loop_end_node in end_nodes:
-            end_nodes.remove(loop_end_node)
-        # 循环结束
-        if self.finished_times >= self.times:
-            # 找到对应的Loop_End
-            executor.finished_loop_id.add(self.loop_id)
-            return [loop_end_node]
-        else:
-            return end_nodes
 
 
-class LoopEnd(Node):
+class Loop_End(Node):
     def __init__(self, loop_id, **kwargs):
-        super().__init__(7, **kwargs)
+        super().__init__(6, **kwargs)
         self.loop_id = loop_id
-
-    def __call__(self, executor: Executor):
-        end_nodes = [edge.end for edge in self.out_edges]
-        loop_node = None
-        for node in end_nodes:
-            if isinstance(node, Loop) and node.loop_id == self.loop_id:
-                loop_node = node
-                break
-        assert loop_node is not None, f'Did not find corresponding loop node for end loop node{self.loop_id}'
-        end_nodes.remove(loop_node)
-        # 退出循环
-        if self.loop_id in executor.finished_loop_id:
-            executor.finished_loop_id.remove(self.loop_id)
-            return end_nodes
-        # 继续下一次循环
-        else:
-            return [loop_node]
 
 
 class Break(Node):
     def __init__(self, loop_id, **kwargs):
-        super().__init__(8, **kwargs)
+        super().__init__(7, **kwargs)
         self.loop_id = loop_id
-
-    def __call__(self, executor: Executor):
-        executor.finished_loop_id.remove(self.loop_id)
-        end_nodes = [edge.end for edge in self.out_edges]
-        loop_end_node = None
-        for node in end_nodes:
-            if isinstance(node, LoopEnd) and node.loop_id == self.loop_id:
-                loop_end_node = node
-                break
-        assert loop_end_node is not None, f'Did not find corresponding loop end node for break node{self.loop_id}'
-        return loop_end_node
 
 
 class If(Node):
     def __init__(self, **kwargs):
+        super().__init__(8, **kwargs)
+
+
+class If_Branch(Node):
+    def __init__(self, **kwargs):
         super().__init__(9, **kwargs)
 
-    def __call__(self, executor: Executor):
-        # for edge in self.out_edges:
-        pass
 
-
-class IfBranch(Node):
+class If_End(Node):
     def __init__(self, **kwargs):
         super().__init__(10, **kwargs)
 
 
-class IfEnd(Node):
+class Assignment(Node):
     def __init__(self, **kwargs):
         super().__init__(11, **kwargs)
 
 
-class Assignment(Node):
+# 算术表达式所用节点
+class Add(Node):
     def __init__(self, **kwargs):
         super().__init__(12, **kwargs)
+
+
+class Sub(Node):
+    def __init__(self, **kwargs):
+        super().__init__(13, **kwargs)
+
+
+class Mul(Node):
+    def __init__(self, **kwargs):
+        super().__init__(14, **kwargs)
+
+
+class Div(Node):
+    def __init__(self, **kwargs):
+        super().__init__(15, **kwargs)
+
+
+class LOG(Node):
+    def __init__(self, **kwargs):
+        super().__init__(16, **kwargs)
+
+
+class POW(Node):
+    def __init__(self, **kwargs):
+        super().__init__(17, **kwargs)
+        self.base = 0
+
+    def set_base(self, base):
+        self.base = base
+
+
+class SQRT(Node):
+    def __init__(self, **kwargs):
+        super().__init__(18, **kwargs)
+
+
+class MATMUL(Node):
+    def __init__(self, **kwargs):
+        super().__init__(19, **kwargs)
+
+
+class DOT(Node):
+    def __init__(self, **kwargs):
+        super().__init__(20, **kwargs)
+
+
+class INNER(Node):
+    def __init__(self, **kwargs):
+        super().__init__(21, **kwargs)
+
+
+class OUTER(Node):
+    def __init__(self, **kwargs):
+        super().__init__(22, **kwargs)
+
+
+class TENSORDOT(Node):
+    def __init__(self, **kwargs):
+        super().__init__(23, **kwargs)
+        self.axes = 2
+
+    def set_axes(self, axes):
+        self.axes = axes
+
+
+class KRON(Node):
+    def __init__(self, **kwargs):
+        super().__init__(24, **kwargs)
+
+
+class CHOLESKY(Node):
+    def __init__(self, **kwargs):
+        super().__init__(25, **kwargs)
+
+
+class QR(Node):
+    def __init__(self, **kwargs):
+        super().__init__(26, **kwargs)
+        self.mode = ''
+
+    def set_mode(self, mode):
+        self.mode = mode
+
+
+class SVD(Node):
+    def __init__(self, **kwargs):
+        super().__init__(27, **kwargs)
+        self.parameter_dict = {'full_matrices': 1, 'compute_uv': 1, 'hermitian': 0}
+
+    def set_param(self, full_matrices, compute_uv, hermitian):
+        self.parameter_dict['full_matrices'] = full_matrices
+        self.parameter_dict['compute_uv'] = compute_uv
+        self.parameter_dict['hermitian'] = hermitian
+
+
+class NORM(Node):
+    def __init__(self, **kwargs):
+        super().__init__(28, **kwargs)
+        self.parameter_dict = {'ord': None, 'axis': None, 'keepdims': 0}
+
+    def set_param(self, ord, axis, keepdims):
+        self.parameter_dict['ord'] = ord
+        self.parameter_dict['axis'] = axis
+        self.parameter_dict['keepdims'] = keepdims
+
+
+class COND(Node):
+    def __init__(self, **kwargs):
+        super().__init__(29, **kwargs)
+        self.parameter_dict = {'p': None}
+
+    def set_param(self, p):
+        self.parameter_dict['p'] = p
+
+
+class DET(Node):
+    def __init__(self, **kwargs):
+        super().__init__(30, **kwargs)
+
+
+class RANK(Node):
+    def __init__(self, **kwargs):
+        super().__init__(31, **kwargs)
+
+
+class TRACE(Node):
+    def __init__(self, **kwargs):
+        super().__init__(32, **kwargs)
+        self.parameter_dict = {'offset': 0, 'axis1': 0, 'axis2': 1, 'dtype': None, 'out': None}
+
+    def set_param(self, offset, axis1, axis2, dtype, out):
+        self.parameter_dict['offset'] = offset
+        self.parameter_dict['axis1'] = axis1
+        self.parameter_dict['axis2'] = axis2
+        self.parameter_dict['dtype'] = dtype
+        self.parameter_dict['out'] = out
+
+
+class RESHAPE(Node):
+    def __init__(self,  **kwargs):
+        super().__init__(33, **kwargs)
+        self.parameter_dict = {'newshape': None, 'order': 'C'}
+
+    def set_param(self, newshape, order):
+        self.parameter_dict['newshape'] = newshape
+        self.parameter_dict['order'] = order
+
+
+class TRANSPOSE(Node):
+    def __init__(self, **kwargs):
+        super().__init__(34, **kwargs)
+
+
+class STACK(Node):
+    def __init__(self, **kwargs):
+        super().__init__(35, **kwargs)
+        self.axis = 0
+
+    def set_axis(self, axis):
+        self.axis = axis
+
+
+class Blank(Node):
+    def __init__(self, **kwargs):
+        super().__init__(36, **kwargs)
+
+
+class Slice(Node):
+    def __init__(self, **kwargs):
+        super().__init__(37, **kwargs)
+        self.slice_info = []
+
+    def set_slice(self, slice_info):
+        self.slice_info += slice_info
 
 
 # 通过globals方法，以类名选择类进行实例化
@@ -193,18 +280,17 @@ def InstantiationClass(nodeId, nodeType, with_grad=False, **otherField):
         data_shape = otherField['data_shape']
         node = globals()[nodeType](data_shape, id=nodeId, with_grad=with_grad)
     elif nodeType == 'Sql':
-        t_info = otherField['t_info']
-        node = globals()[nodeType](t_info, id=nodeId, with_grad=with_grad)
+        t_name = otherField['t_name']
+        node = globals()[nodeType](t_name, id=nodeId, with_grad=with_grad)
     elif nodeType == 'Random':
-        boundary = otherField['boundary']
-        data_shape = otherField['data_shape']
-        type = otherField['type']
-        node = globals()[nodeType](boundary, data_shape, type, id=nodeId, with_grad=with_grad)
+        uLimit = otherField['uLimit']
+        lLimit = otherField['lLimit']
+        node = globals()[nodeType](uLimit, lLimit, id=nodeId, with_grad=with_grad)
     elif nodeType == 'Loop':
         times = otherField['times']
         loop_id = otherField['loop_id']
         node = globals()[nodeType](times, loop_id, id=nodeId, with_grad=with_grad)
-    elif nodeType == 'LoopEnd' or nodeType == 'Break':
+    elif nodeType == 'Loop_End' or nodeType == 'Break':
         loop_id = otherField['loop_id']
         node = globals()[nodeType](loop_id, id=nodeId, with_grad=with_grad)
     elif nodeType == 'Symbol':
@@ -213,3 +299,4 @@ def InstantiationClass(nodeId, nodeType, with_grad=False, **otherField):
     else:
         node = globals()[nodeType](id=nodeId, with_grad=with_grad)
     return node
+
