488085b30f9a92506eb0b4f2e58aea5a2fcd0b85
zkx741481546 <zkx741481546@live.com>
2021-03-30 19:47:14 +0800

    修复逻辑回归代码

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
 import torch
-a = torch.ones((3,4))
-b=a[0,:]
-# class mytensor:
-#     def __init__(self):
-#         pass
-#     def __getitem__(self, item):
-#         pass
-# c = mytensor()
-# c[0,0:3]
-if b._base is not None:
-    print(b._base)
 
-    a.__setitem__((0,0),2)
-    print(b)
-    print(b._base)
-else:
-    print('none')
+x = torch.ones((100, 4))
+w = torch.randn((4, ))
+print(torch.matmul(x, w).shape)
