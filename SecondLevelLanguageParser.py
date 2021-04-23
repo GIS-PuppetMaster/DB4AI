90860787137ac313332abdae00501f50dc44a044
zkx741481546 <zkx741481546@live.com>
2021-04-23 12:46:13 +0800

    加入KNN测试代码

--- SecondLevelLanguageParser.py
+++ SecondLevelLanguageParser.py
@@ -722,7 +722,7 @@ class Parser:
 
 if __name__ == '__main__':
     from time import time
-    with open('operators/KNN.sql', 'r') as f:
+    with open('test/KNN.sql', 'r') as f:
         create_test = f.readlines()
     testPar = Parser(create_test)
     result = testPar()
