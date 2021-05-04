e0e714a3ce5ac84ded12c21fa5c0b8f9690e06ee
zkx741481546 <zkx741481546@live.com>
2021-05-04 17:51:06 +0800

    修复： 1、test/KNN补全

--- test/KNN.sql
+++ test/KNN.sql
@@ -10,6 +10,16 @@ loop(1000){
     select i+1 as i
 }
 create tensor test_x(10,3) from random((10,3),(0,1))
+create tensor test_y(10,1) from zeros((10,1))
+select Argmax(test_x, 1) as tmp
+select 0 as i
+loop(10){
+    select tmp[i] as t
+    if(t>1 and t<3){
+        select 1 as test_y[i,...]
+    }
+    select i+1 as i
+}
 create tensor k(1,) from 10
 create tensor mse(1,)
 create tensor auc(1,)
@@ -17,4 +27,4 @@ create tensor f1(1,)
 create tensor acc(1,)
 create tensor recall(1,)
 create tensor prec(1,)
-select KNN(acc,auc,prec,recall,mse,f1, test_x, x, y, k)
+select KNN(acc,auc,prec,recall,mse,f1, test_x, test_y, x, y, k)
