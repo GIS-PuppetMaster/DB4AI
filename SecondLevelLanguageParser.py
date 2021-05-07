4bbbd43c43589d2d7b21b61e7d35b8bff5cc07a3
lu <2916867200@qq.com>
2021-05-07 17:15:12 +0800

    修改自定义算子内部图中val类vars的前缀

--- SecondLevelLanguageParser.py
+++ SecondLevelLanguageParser.py
@@ -805,7 +805,7 @@ class Parser:
 
 if __name__ == '__main__':
     from time import time
-    path = 'test.txt'
+    path = 'operators/KNN.sql'
     with open(path, 'r', encoding='utf-8') as f:
         create_test = f.readlines()
     testPar = Parser(create_test)
