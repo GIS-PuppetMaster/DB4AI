29a71cae9dae9d7ad0fef7d311e3f9de983c51c6
zkx74 <zkx741481546@live.com>
2021-11-16 16:22:06 +0800

    使用真实数据测试logistic的脚本

--- test/logistic.sql
+++ test/logistic.sql
@@ -1,30 +1,11 @@
-create tensor x(10,4) from random((10,4),(0,1))
-create tensor y(10,1) from zeros((10,1))
-select Argmax(x, 1) as tmp
-create tensor i(1,) from 0
-create tensor temp(1,) from 0
-loop(10){
-    select tmp[i] as t
-    if(t>1){
-        select 1 as y[i,...]
-    }
-    select i+1 as i
-}
-create tensor test_x(10,4) from random((10,4),(0,1))
-create tensor test_y(10,1) from zeros((10,1))
-select Argmax(test_x, 1) as tmp
-select 0 as i
-loop(10){
-    select tmp[i] as t
-    if(t>1){
-        select 1 as test_y[i,...]
-    }
-    select i+1 as i
-}
+select SQL('select feature_1,feature_2,feature_3 from train_data') as train_x
+select SQL('select label from train_data') as train_y
+select SQL('select feature_1,feature_2,feature_3 from test_data') as test_x
+select SQL('select label from test_data') as test_y
 create tensor lr(1,) from 0.01
 create tensor class_num(1,) from 2
-create tensor ridge(1,) from 0.01
-create tensor iter_times(1,) from 1000
+create tensor ridge(1,) from 0.0001
+create tensor iter_times(1,) from 50
 create tensor mse(1,)
 create tensor auc(1,)
 create tensor f1(1,)
